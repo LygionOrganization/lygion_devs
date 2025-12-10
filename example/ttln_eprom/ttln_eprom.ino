c36df25825eefa9f4bcf2df79d03647c70ba02d4
ftservo <117692731@qq.com>
2025-12-10 23:26:12 +0800

    example

--- example/ttln_eprom/ttln_eprom.ino
+++ example/ttln_eprom/ttln_eprom.ino
@@ -8,9 +8,10 @@ void setup()
   //Serial1.begin(1000000);//mega2560
   node.pSerial = &Serial1;
   delay(1000);
-  node.unLockEprom(1);//打开EPROM保存功能
-  node.writeByte(1, LY_NODE_ID, 2);//ID
-  node.LockEprom(2);//关闭EPROM保存功能
+  node.unLockEprom(254);//打开EPROM保存功能
+  node.writeByte(254, LY_NODE_ID, 0);//ID
+  delay(200);
+  node.LockEprom(254);//关闭EPROM保存功能
 }
 
 void loop()
